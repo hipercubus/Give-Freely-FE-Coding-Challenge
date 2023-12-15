2fc565ed645394bff55ea18d247559980d0ec420
Christian Motta <cm@castinellc.com>
2023-12-14 23:28:47 -0300

    Add results highlights script

--- contents/results.ts
+++ contents/results.ts
@@ -1,1 +1,26 @@
-//TODO: Highlight results that match with fetched websites
+import type { PlasmoCSConfig } from "plasmo"
+
+import { getWebsitesFromBG } from "~lib/background"
+
+export const config: PlasmoCSConfig = {
+  matches: ["https://*.google.com/search*"],
+  all_frames: true
+}
+
+getWebsitesFromBG().then(({ websites }) => {
+  const selectorsList = websites.map(({ url }) => `span:has(a[href*='${url}'])`)
+  const selectorsString = selectorsList.join(", ")
+  const anchorsList = document.querySelectorAll(selectorsString)
+
+  //TODO: improve the selector instead of using parentElement.parentElement...
+  //TODO: find the way of using a class instead of inline styles
+  anchorsList.forEach((anchor) => {
+    const linkContainer =
+      anchor.parentElement.parentElement.parentElement.parentElement
+    linkContainer.style.border = "1px solid #b1e8b3"
+    linkContainer.style.padding = "10px"
+    linkContainer.style.borderRadius = "5px"
+    linkContainer.style.background =
+      "linear-gradient(45deg, rgb(177 232 179 / 20%), transparent)"
+  })
+})
--- lib/background.ts
+++ lib/background.ts
@@ -4,6 +4,5 @@ export const getWebsitesFromBG = async () => {
   const resp: WebsitesResponse = await sendToBackground({
     name: "websites"
   })
-  console.log("🔎  >> messageFromBackground  >> resp.data:", resp)
   return resp
 }
